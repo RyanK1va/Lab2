d8460ea5439238b83460c360af4bfd6eacecd5d3
Ryan Tan <ryantanlw06.25@ichat.sp.edu.sg>
2026-05-06 09:37:47 +0800

    Lab2 Question 6

---
+++ bmi.py
@@ -0,0 +1,15 @@
+weight = 70  # kg
+height = 1.75  # meters
+# YOUR CODE HERE
+weight = float(input("Enter yr weight(kg):"))
+height = float(input("Enter yr height(m):"))
+BMI = weight/(height * height)
+print(f"BMI=",BMI)
+if BMI < 18.5:
+    print(f"Category: Underweight",BMI)
+elif BMI < 24.9:
+    print(f"Category: Normal")
+elif BMI < 29.9:
+    print(f"Category: Overweight")
+else:
+    print(f"Category: Obese")
